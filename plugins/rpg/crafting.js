c01b135daa6fc32cb05d088ecd302b43a0f61842
MaouDabi <erkipandial@gmail.com>
2025-09-27 12:20:43 +0700

    Update Dabi-Ai
    update 1.3.8

--- plugins/rpg/crafting.js
+++ plugins/rpg/crafting.js
@@ -1,6 +1,8 @@
-import fs from 'fs';
-import path from 'path';
-const gameFile = path.resolve('./toolkit/db/game.json');
+import fs from 'fs'
+import path from 'path'
+
+const gameFile = path.resolve('./toolkit/db/game.json')
+const settingPath = path.resolve('./toolkit/set/settingGame.json')
 
 export default {
   name: 'crafting',
@@ -10,79 +12,74 @@ export default {
   prefix: true,
   premium: false,
 
-  run: async (conn, msg, {
-    chatInfo,
-    args
-  }) => {
-    const { chatId, senderId } = chatInfo;
+  run: async (conn, msg, { chatInfo, args }) => {
+    const { chatId, senderId } = chatInfo
 
     try {
-      const { ore } = await global.loadFunctions();
-      const db = JSON.parse(fs.readFileSync(gameFile));
-      const users = db.tca?.user || {};
-      const nama = Object.keys(users).find(k => users[k].id === senderId);
+      const setting = JSON.parse(fs.readFileSync(settingPath))
+      const oreList = setting.setGame?.rpg?.ore || {}
+      const woodList = setting.setGame?.rpg?.wood || {}
+
+      const db = JSON.parse(fs.readFileSync(gameFile))
+      const users = db.tca?.user || {}
+      const nama = Object.keys(users).find(k => users[k].id === senderId)
 
       if (!nama) {
         return conn.sendMessage(chatId, {
           text: `Kamu belum mempunyai akun game!\nKetik *.create <nama>* untuk membuat akun.`
-        }, { quoted: msg });
+        }, { quoted: msg })
       }
 
       if (args.length < 2) {
         return conn.sendMessage(chatId, {
           text: `Format salah!\nContoh: *.buat pickaxe iron*`
-        }, { quoted: msg });
+        }, { quoted: msg })
       }
 
-      const u = users[nama];
-      const inv = u.inv = u.inv || {};
-      const [tool, mat] = args;
-      const bahan = mat.toLowerCase();
+      const u = users[nama]
+      const inv = (u.inv = u.inv || {})
+      const [tool, mat] = args
+      const bahan = mat.toLowerCase()
 
-      const dataMat = ore.mat.find(([_, n]) => n === bahan);
-      if (!dataMat) {
+      if (!oreList[bahan]) {
         return conn.sendMessage(chatId, {
           text: `Material ${bahan} tidak dikenali.`
-        }, { quoted: msg });
+        }, { quoted: msg })
       }
 
-      const durability = dataMat[2] || 1000;
-      const kayuKey = 'wood';
-      const oreKey = 'ore';
+      const durability = oreList[bahan].str * 100 || 1000
 
-      const kayuAda = Object.values(inv.wood || {}).reduce((a, b) => a + b, 0) > 0;
-      const bahanAda = (inv.ore?.[bahan] || 0) > 0;
+      const kayuAda = Object.values(inv.wood || {}).reduce((a, b) => a + b, 0) > 0
+      const bahanAda = (inv.ore?.[bahan] || 0) > 0
 
       if (!kayuAda || !bahanAda) {
         return conn.sendMessage(chatId, {
           text: `Bahan tidak cukup!\nDibutuhkan: 1 ${bahan} (ore) & 1 kayu`
-        }, { quoted: msg });
+        }, { quoted: msg })
       }
 
-      inv.ore[bahan]--;
-      if (inv.ore[bahan] === 0) delete inv.ore[bahan];
+      inv.ore[bahan]--
+      if (inv.ore[bahan] === 0) delete inv.ore[bahan]
 
-      const woodTypes = Object.keys(inv.wood || {});
+      const woodTypes = Object.keys(inv.wood || {})
       if (woodTypes.length) {
-        const w = woodTypes[0];
-        inv.wood[w]--;
-        if (inv.wood[w] === 0) delete inv.wood[w];
+        const w = woodTypes[0]
+        inv.wood[w]--
+        if (inv.wood[w] === 0) delete inv.wood[w]
       }
 
-      inv[tool] = inv[tool] || {};
-      inv[tool][bahan] = { durabilty: durability };
+      inv[tool] = inv[tool] || {}
+      inv[tool][bahan] = { durability }
 
-      fs.writeFileSync(gameFile, JSON.stringify(db, null, 2));
+      fs.writeFileSync(gameFile, JSON.stringify(db, null, 2))
 
       conn.sendMessage(chatId, {
         text: `Berhasil membuat ${tool}.${bahan} dengan durabilitas ${durability}.\nBahan telah dikurangi.`
-      }, { quoted: msg });
+      }, { quoted: msg })
 
     } catch (err) {
-      console.error(err);
-      conn.sendMessage(chatId, {
-        text: 'Terjadi kesalahan saat crafting!'
-      }, { quoted: msg });
+      console.error(err)
+      conn.sendMessage(chatId, { text: 'Terjadi kesalahan saat crafting!' }, { quoted: msg })
     }
   }
-};
+}
