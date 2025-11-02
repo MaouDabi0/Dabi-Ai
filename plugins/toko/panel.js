d5a1747afa795e5fa4a9adab318f7f778697188d
MaouDabi <erkipandial@gmail.com>
2025-11-02 14:26:05 +0700

    Update panel.js

--- plugins/toko/panel.js
+++ plugins/toko/panel.js
@@ -1,34 +1,34 @@
-import fs from 'fs';
+import fs from 'fs'
 
 export default {
   name: 'panel',
   command: ['panel'],
   tags: 'Toko Menu',
   desc: 'Menampilkan toko panel',
+  prefix: !0,
+  owner: !1,
+  premium: !1,
 
-  run: async (conn, msg, { chatInfo }) => {
-    const { chatId } = chatInfo;
-    const tokoPath = './toolkit/set/toko.json';
-
-    let tokoData;
+  run: async (conn, msg, {
+    chatInfo
+  }) => {
+    const { chatId } = chatInfo,
+          tokoPath = './toolkit/set/toko.json'
+    let tokoData
     try {
-      tokoData = JSON.parse(fs.readFileSync(tokoPath, 'utf-8'));
+      tokoData = JSON.parse(fs.readFileSync(tokoPath, 'utf-8'))
     } catch {
-      return conn.sendMessage(chatId, { text: "Gagal membaca file toko.json" }, { quoted: msg });
+      return conn.sendMessage(chatId, { text: 'Gagal membaca file toko.json' }, { quoted: msg })
     }
 
-    const items = tokoData.storeSetting['panel'];
-    if (!items?.length) {
-      return conn.sendMessage(chatId, { text: "Toko ini belum memiliki barang." }, { quoted: msg });
-    }
-
-    const itemList = items
-      .sort((a, b) => a.name.localeCompare(b.name))
-      .map((item, i) => `${i + 1}. ${item.name}: ${item.price}`)
-      .join('\n');
-
-    conn.sendMessage(chatId, { 
-      text: `Daftar barang di toko panel:\n\n${itemList}`
-    }, { quoted: msg });
+    const items = tokoData.storeSetting['panel']
+    return !items?.length
+      ? conn.sendMessage(chatId, { text: 'Toko ini belum memiliki barang.' }, { quoted: msg })
+      : conn.sendMessage(chatId, {
+          text: `Daftar barang di toko panel:\n\n${items
+            .sort((a,b)=>a.name.localeCompare(b.name))
+            .map((v,i)=>`${i+1}. ${v.name}: ${v.price}`)
+            .join('\n')}`
+        }, { quoted: msg })
   }
-};
+}
